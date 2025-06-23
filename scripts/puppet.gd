03b12351eb9b4cd6888c6be69789ad138d621b35
Hewson Schulz <hewsonschulz@gmail.com>
2025-06-22 22:40:17 -0500

    diagonal puppet movement

--- scripts/puppet.gd
+++ scripts/puppet.gd
@@ -1,40 +1,30 @@
 extends CharacterBody2D
 
-
 @export_range(0.0, 1.0, 0.05) var speed: float = 0.85
 const STEP_SIZE: int = 16
 var move_timer := 0.0
 var ready_to_move := true
 
-
 func _ready():
 	global_position = global_position.snapped(Vector2(STEP_SIZE, STEP_SIZE))
-	print("move_delay: ", get_move_delay())
 
 func get_move_delay() -> float:
 	return 1.0 - speed
 
 func _physics_process(delta):
-	# Countdown between held moves
 	if not ready_to_move:
 		move_timer -= delta
 		if move_timer <= 0:
 			ready_to_move = true
 
 	if ready_to_move:
-		var input_vector := Vector2.ZERO
-
-		if Input.is_action_pressed("right"):
-			input_vector.x += 1
-		elif Input.is_action_pressed("left"):
-			input_vector.x -= 1
-		elif Input.is_action_pressed("down"):
-			input_vector.y += 1
-		elif Input.is_action_pressed("up"):
-			input_vector.y -= 1
+		var input_vector := Vector2(
+			int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left")),
+			int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+		)
 
 		if input_vector != Vector2.ZERO:
-			global_position += input_vector.normalized().snapped(Vector2.ONE) * STEP_SIZE
+			input_vector = input_vector.normalized().snapped(Vector2.ONE)
+			global_position += input_vector * STEP_SIZE
 			move_timer = get_move_delay()
-		
 			ready_to_move = false
