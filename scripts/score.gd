783118597f48e8e4c1738c158294293597137a32
Hewson Schulz <hewsonschulz@gmail.com>
2025-06-23 17:01:18 -0500

    audio assets, minor audio implementation

--- scripts/score.gd
+++ scripts/score.gd
@@ -1,20 +1,40 @@
 extends Node
 
+
 @onready var num1 = $AnimatedSprite1
 @onready var num10 = $AnimatedSprite10
 @onready var num100 = $AnimatedSprite100
 @onready var num1000 = $AnimatedSprite1000
 @onready var puppet = %Puppet
+@onready var crying_child = %CryingChild
+@onready var jumpscare = %Jumpscare
 var score: int = 0
+@onready var sound = $"../Place"
+
 
 func increment(inc = 100):
 	score += inc
 	score = clamp(score, 0, 9999) # max score of 9999
 	refresh_display()
 	
-	# modify puppet speed
 	if score > 400:
+		# modify puppet speed
 		puppet.speed -= 0.1
+	
+	if score < 800:
+		if score < 500:
+			sound.play()
+		else:
+			sound.pitch_scale = 0.2
+			sound.play()
+	else:
+		# end game
+		crying_child.get_node("Sprite2D").visible = true
+		
+		await get_tree().create_timer(0.05).timeout
+		
+		jumpscare.visible = true
+		jumpscare.play()
 
 func refresh_display():
 	var score_str := str(score).pad_zeros(4)
