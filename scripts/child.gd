293b5caf1086f824cb291156f731384ea6633454
Hewson Schulz <hewsonschulz@gmail.com>
2025-06-23 00:11:56 -0500

    gift giving functionality

---
+++ scripts/child.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var gift_node: NodePath
+var gifted: bool = false
+
+func _on_body_entered(body):
+	if gifted:
+		return
+
+	gifted = true
+
+	var gift = get_node(gift_node)
+	if gift:
+		gift.visible = true
