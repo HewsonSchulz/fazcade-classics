07384e0c24607dbc30239abf4fe31b72eb2a26fb
Hewson Schulz <hewsonschulz@gmail.com>
2025-06-23 09:25:40 -0500

    score setup

--- scripts/child.gd
+++ scripts/child.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var gift_node: NodePath
 var gifted: bool = false
+@onready var score = %Score
 
 func _on_body_entered(_body):
 	if gifted:
@@ -12,3 +13,4 @@ func _on_body_entered(_body):
 	var gift = get_node(gift_node)
 	if gift:
 		gift.visible = true
+	score.increment()
---
+++ scripts/score.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+var score: int = 0
+
+func increment(num=100):
+	score += num
